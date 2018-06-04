2e71b1c0881e95dffaa18e0762aa08179f7c60ed
Aron van Groningen <aronvgroningen@gmail.com>
2018-06-04 10:14:08 +0200

    update Pure data

--- first_test_file.py
+++ first_test_file.py
@@ -1,6 +1,10 @@
 import requests
 import json
 import pprint
+from rdflib import Graph, RDF, Namespace, Literal, URIRef
+from rdflib import URIRef, BNode, Literal
+from rdflib.namespace import RDF, FOAF
+
 
 data = {"data" : "24.3"}
 data_json = json.dumps(data)
@@ -8,164 +12,200 @@ data_json = json.dumps(data)
 
 headers = {'Accept': 'application/json'}
 
+#data from PURE system
 resp_person = requests.get("https://research.vu.nl/ws/api/59/persons?q=harmelen&apiKey=1aecc9b3-0b58-4e00-b757-c1a8026cbbfd",  headers=headers)
 response_person = resp_person.json()
-# pprint.pprint(response_person)
+
+# data_person = response_person               #data from scopus
+
+#data from JSON file
+with open('data_pure_author.json') as json_file:
+    data_json_file = json.load(json_file)
+
+data_person = data_json_file       #data from JSON file
+
+# with open('data_pure_author.json', 'w') as outfile:
+#     json.dump(response_person, outfile)
+
+# pprint.pprint(data_person)
+
+
+
+# def serialize(filename):
+#     g.serialize(destination=filename, format='turtle')
+#     print("File is saved")
+
+def save(filename):
+    with open(filename, 'a+') as f:
+        g.serialize(destination=filename, format='turtle')
+        print("File is saved")
+
+
+def load(filename):
+    with open(filename, 'r') as f:
+        g.load(f, format='turtle')
+
+
+# Initialize author with uri
+URI = "https://krr.cs.vu.nl/"
+
+def transformToRDF(strURI):
+
+    if strURI[0:] == '_':
+        strURI = strURI[0:].replace("_", "")  # delete     (does not work)
+
+    strURI = strURI.replace(":", "") #delete :
+    strURI = strURI.replace("/", "")  #delete /
+    strURI = strURI.replace(" ", "_") #replace the space into
+    strURI = strURI.replace(",", "") #delete ,
+    strURI = strURI.replace("'", "") #delete single quote       (does not work)
+    strURI = strURI.replace("''", "") #delete dubble quote      (does not work)
+    strURI = strURI.replace("""""", "") #delete dubble quote    (does not work)
+    strURI = strURI.replace("(", "") #delete (
+    strURI = strURI.replace(")", "") #delete )
+    strURI = strURI.replace("-", "_") #delete -
+    strURI = strURI.replace("?", "") #delete ?
+    strURI = strURI.replace(".", "_") #delete .
+    strURI = strURI.replace(";", "") #delete ;
+    strURI = strURI.replace("©", "") #delete ©
+    strURI = strURI.replace("%", "") #delete %
+    strURI = strURI.replace("=", "") #delete =
+    strURI = strURI.replace("!", "") #delete !
+    strURI = strURI.replace("[", "") #delete .
+    strURI = strURI.replace("]", "") #delete .
+    strURI = strURI.replace("é", "e") #delete .
+
+    return URIRef(URI + strURI) #create URI
+
 
 #Details author
-title = response_person['items'][0]['titles'][0]['value']
-firstName = response_person['items'][0]['name']['firstName']
-lastName = response_person['items'][0]['name']['lastName']
-scopusID = response_person['items'][0]['ids'][1]['value']
-pureID = response_person['items'][0]['uuid']
+title = data_person['items'][0]['titles'][0]['value']
+firstName = data_person['items'][0]['name']['firstName']
+lastName = data_person['items'][0]['name']['lastName']
+scopusID = data_person['items'][0]['ids'][1]['value']
+pureID = data_person['items'][0]['uuid']
+
 
 lastName = lastName.replace(" ","_")
 firstName = firstName.replace(" ","_")
 title = title.replace(" ","_")
 pureID = str(pureID)
 
+
+
 print(scopusID)
-print(pureID)
-resp_research_outputs = requests.get('https://research.vu.nl/ws/api/59/persons/'+ pureID +'/research-outputs?apiKey=1aecc9b3-0b58-4e00-b757-c1a8026cbbfd',  headers=headers)
+# print(pureID)
+
+
+
+#============================
+# RESEARCH OUTPUT
+#============================
+# resp_publications = requests.get('https://research.vu.nl/ws/api/59/persons/'+ pureID +'/research-outputs?apiKey=1aecc9b3-0b58-4e00-b757-c1a8026cbbfd',  headers=headers)
+# response_publications = resp_publications.json()
+# data_publications = response_publications               #data from Pure
+#
+
+#data from JSON file
+with open('data_pure_publications.json') as json_file:
+    data_json_file = json.load(json_file)
+data_publications = data_json_file       #data from JSON file
+
+# with open('data_pure_publications.json', 'w') as outfile:
+#     json.dump(response_publications, outfile)
+pprint.pprint(data_publications)
+
 
 if resp_person.status_code != 200:
     # This means something went wrong.
     print('life is hard')
     raise ApiError('GET /tasks/ {}'.format(resp_person.status_code))
 
-response_research_outputs = resp_research_outputs.json()
-# pprint.pprint(response_research_outputs)
 
-totalPublications = response_research_outputs['count']
-publicationsItems = response_research_outputs['items']
+totalPublications = data_publications['count']
+publicationsItems = data_publications['items']
 
-# print publicationsItems
-print(len(publicationsItems))
-pprint.pprint(publicationsItems)
-
-#no.1
-print(publicationsItems[0]['title'])
-print(publicationsItems[0]['electronicVersions'][1]['doi'])
-
+publications = []
 print(len(publicationsItems))
 for i in range(len(publicationsItems)):
 
-    print (i)
-    print (publicationsItems[i]['title'])
-    print (publicationsItems[i]['electronicVersions'][0]['file']['fileURL'])
+    publications.append({
+        'title': publicationsItems[i]['title'],
+        # 'issn': publicationsItems['journalAssociation']['issn'],
+        # 'pages': publicationsItems['pages'],
+        # 'fileURL': publicationsItems[i]['electronicVersions'][0]['file']['fileURL'],
+        'doi': publicationsItems[0]['electronicVersions'][1]['doi']
 
-# [{u'created': u'2017-02-05T12:55:32.326+0000', u'creator': u'root', u'accessType': [{u'uri': u'/dk/atira/pure/core/openaccesspermission/open', u'value': u'Open'}], u'visibleOnPortalDate': u'2017-02-05T12:55:32.326+0000', u'file': {u'mimeType': u'application/pdf', u'digestAlgorithm': u'SHA1', u'fileName': u'swj588 0.pdf', u'fileURL': u'https://research.vu.nl/ws/files/666163/swj588%200.pdf', u'digest': u'9C9F042A242D74C01FC69DD44DFA57EA27070FC0', u'size': 307701}, u'versionType': [{u'uri': u'/dk/atira/pure/publication/electronicversion/versiontype/publishersversion', u'value': u'Final published version'}], u'id': 666162},
-#  {u'accessType': [{u'uri': u'/dk/atira/pure/core/openaccesspermission/unknown', u'value': u'Unknown'}], u'doi': u'http://dx.doi.org/10.3233/SW-140158', u'visibleOnPortalDate': u'2017-02-05T12:55:32.326+0000', u'id': 666164, u'versionType': [{u'uri': u'/dk/atira/pure/publication/electronicversion/versiontype/publishersversion', u'value': u'Final published version'}]}]
-
+    })
 
 
 #==============================
 # Extra Information from PURE
 #==============================
 # #IDS
-# print(response_person['items'][0]['externalableInfo']['secondarySources']) #Same ID a Scopus ID
-# print(response_person['items'][0]['ids']) #Second element [1] is the same as the scopus ID
-# id_person = response_person['items'][0]['uuid'] #Person-ID of Pure?
-#
+print(data_person['items'][0]['externalableInfo']['secondarySources']) #Same ID a Scopus ID
+print(data_person['items'][0]['ids']) #Second element [1] is the same as the scopus ID
+id_person = data_person['items'][0]['uuid'] #Person-ID of Pure?
+
 #     #Name Variance
-# print(response_person['items'][0]['nameVariants'])
+print(data_person['items'][0]['nameVariants'])
 # #Name Title
-# print(response_person['items'][0]['titles'])
+print(data_person['items'][0]['titles'])
 #
 # #E-mail
-# print(response_person['items'][0]['staffOrganisationAssociations'][0]['emails'][0]['value'])
+print(data_person['items'][0]['staffOrganisationAssociations'][0]['emails'][0]['value'])
 
 
 #Keywords workfield?
-# print(response_person['items'][0]['keywordGroups'][0]['keywords'])
+print(data_person['items'][0]['keywordGroups'][0]['keywords'])
 
 
 
 # #=========================
 # # Publications
 # #========================
-# resp_publications = requests.get("https://research.vu.nl/ws/api/59/persons/" + id_person +"/research-outputs?apiKey=1aecc9b3-0b58-4e00-b757-c1a8026cbbfd",  headers=headers)
-#
-#
-# resp_publications = requests.get("https://research.vu.nl/ws/api/59/persons/b36edb65-746c-46be-9ecb-e532eb23c24e/research-outputs?apiKey=1aecc9b3-0b58-4e00-b757-c1a8026cbbfd",  headers=headers)
-# if resp_publications.status_code != 200:
-#     # This means something went wrong.
-#     print('life is hard')
-#     raise ApiError('GET /tasks/ {}'.format(resp_publications.status_code))
-#
-# response_publications = resp_publications.json()
-# pprint.pprint(response_publications)
-#
-#
-# print (response_publications['count'])
-#
-# print (response_publications['items'])
+resp_publications = requests.get("https://research.vu.nl/ws/api/59/persons/" + id_person +"/research-outputs?apiKey=1aecc9b3-0b58-4e00-b757-c1a8026cbbfd",  headers=headers)
 
+
+resp_publications = requests.get("https://research.vu.nl/ws/api/59/persons/b36edb65-746c-46be-9ecb-e532eb23c24e/research-outputs?apiKey=1aecc9b3-0b58-4e00-b757-c1a8026cbbfd",  headers=headers)
+if resp_publications.status_code != 200:
+    # This means something went wrong.
+    print('life is hard')
+    raise ApiError('GET /tasks/ {}'.format(resp_publications.status_code))
+
+response_publications = resp_publications.json()
+pprint.pprint(response_publications)
+
+
+print (response_publications['count'])
+
+print (response_publications['items'])
 
 
 #=======================
-#RDFLIB
+# RDF Lib
 #=======================
+g = Graph()
 
-# from rdflib import Graph, RDF, Namespace, Literal, URIRef
+EX = Namespace('https://krr.cs.vu.nl/')
+g.bind('krr', EX)
 
-# g = Graph()
-#
-# EX = Namespace('http://example.com/KE4KE/')
-# g.bind('ex', EX)
-
-# rdfLastName = URIRef("http://example.com/KE4KE/" + scopusID)
+hasScopusID = transformToRDF('hasScopusID')
+hasPureID = transformToRDF('hasPureID')
 
 
-# from rdflib import URIRef, BNode, Literal
-#
-# krr_url = 'https://krr.cs.vu.nl/'
-#
-# title = URIRef(krr_url + title)
-# firstName = URIRef(krr_url + firstName)
-# lastName = URIRef(krr_url + lastName)
-# scopusID = URIRef(krr_url + scopusID)
-# pureID = URIRef(krr_url + pureID)
-#
-# hasScopusID = URIRef("https://krr.cs.vu.nl/hasScopusID")
-# hasPureID = URIRef("https://krr.cs.vu.nl/hasPureID")
-#
-# from rdflib.namespace import RDF, FOAF
-#
-#
-# # RDF.type = rdflib.term.URIRef(u'http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
-#
-# # FOAF.knows = rdflib.term.URIRef(u'http://xmlns.com/foaf/0.1/knows')
-#
-#
-# from rdflib import Graph
-# g = Graph()
-#
-# g.add( (lastName, RDF.type, FOAF.Person) )
-# g.add( (lastName, FOAF.title, title) )
-# g.add( (lastName, FOAF.firstName, firstName) )
-# g.add( (lastName, FOAF.lastName, lastName) )
-# g.add( (lastName, hasScopusID, scopusID) )
-# g.add( (lastName, hasPureID, pureID) )
-#
-#
-# # print (g.serialize(format='turtle'))
-#
-#
-# file = open("data.ttl", mode="w")
-#
-# def serialize(filename):
-#     g.serialize(destination=filename, format='turtle')
-#     print("File is saved")
-#
-# def save(filename):
-#     with open(filename, 'w') as f:
-#         g.serialize(f, format='turtle')
-#
-#
-# def load(filename):
-#     with open(filename, 'r') as f:
-#         g.load(f, format='turtle')
-#
-#
-# serialize('data.ttl')
+title = transformToRDF(title)
+firstName = transformToRDF(firstName)
+lastName = transformToRDF(lastName)
+scopusID = transformToRDF(scopusID)
+pureID = transformToRDF(pureID)
+
+g.add( (lastName, RDF.type, FOAF.Person) )
+g.add( (lastName, FOAF.title, title) )
+g.add( (lastName, FOAF.firstName, firstName) )
+g.add( (lastName, FOAF.lastName, lastName) )
+g.add( (lastName, hasScopusID, scopusID) )
+g.add( (lastName, hasPureID, pureID) )
+
+
+save("data.ttl")
